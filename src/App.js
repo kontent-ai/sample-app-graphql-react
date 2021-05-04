431d3eda853e9f4309cf444e81c116f3c3dd7794
ondrejch <Simply007@users.noreply.github.com>
2021-05-04 15:45:41 +0200

    prepare sceleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./Home";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home itemCodename="main_content" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/KontentDeliveryClient.js
@@ -0,0 +1,7 @@
+import { DeliveryClient } from '@kentico/kontent-delivery';
+
+const deliveryClient = new DeliveryClient({
+  projectId: 'ad25961e-f934-01dc-e1fa-f4dd41b84df2',
+});
+
+export default deliveryClient;
