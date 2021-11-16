0a165483f96feb57850dc5f70ef6b87ece679864
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-11-16 15:49:01 +0100

    fix post detail

--- src/Post.js
+++ src/Post.js
@@ -1,110 +1,116 @@
 import get from "lodash.get";
 import { Image, Layout, RichText, GraphQLLoader } from "./components";
 import { Container, makeStyles, Typography, useTheme } from "@material-ui/core";
-import React, { useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { assetFields, seoFields, richTextFields } from './graphQLFragments';
-import getSeo from './utils/getSeo';
+import React, { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { assetFields, seoFields, richTextFields } from "./graphQLFragments";
+import getSeo from "./utils/getSeo";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        paddingTop: theme.spacing(4)
-    }
+  root: {
+    paddingTop: theme.spacing(4),
+  },
 }));
 
 function Post(props) {
-    const postPageQuery = gql`
-        query PostPageQuery($codename: String!){
-            post(codename: $codename) {
-                seo {
-                    ...SeoFields
-                }
-                _system_ {
-                    type {
-                        _system_ {
-                            codename
-                        }
-                    }
-                }
-                image {
-                    ...AssetFields
-                }
-                title
-                publishingDate
-                author(limit: 1) {
-                    items {
-                        ... on Author {
-                            firstName
-                            lastName
-                        }
-                    }
-                }
-                subtitle
-                content {
-                    ...RichTextFields
-                }
+  const postPageQuery = gql`
+    query PostPageQuery($codename: String!) {
+      post(codename: $codename) {
+        _seo {
+          ...SeoFields
+        }
+        _system_ {
+          type {
+            _system_ {
+              codename
             }
+          }
         }
-
-        ${seoFields}
-        ${assetFields}
-        ${richTextFields}
-    `;
-
-    const [post, setPost] = useState(null);
-    const [seo, setSeo] = useState(null);
-    const classes = useStyles();
-    const theme = useTheme();
-    const imageSizes = `${theme.breakpoints.values.md}px`;
-
-
-    const { loading, error } = useQuery(postPageQuery, {
-        variables: { codename: props.codename },
-        onCompleted: (data) => {
-            setPost(data.post);
-            setSeo(getSeo(data.post.seo))
+        image {
+          ...AssetFields
         }
-    }, [props.codename]);
-
-    if (error || loading || !post) {
-        return <GraphQLLoader error={error} loading={loading} />;
+        title
+        publishingDate
+        author {
+          firstName
+          lastName
+        }
+        subtitle
+        content {
+          ...RichTextFields
+        }
+      }
     }
 
-    return (
-        <Layout {...props} seo={seo}>
-            <Container className={classes.root} maxWidth="md">
-                {get(post, "title", null) && (
-                    <Typography variant="h1">{get(post, "title", null)}</Typography>
-                )}
-                {get(post, "subtitle", null) && (
-                    <Typography variant="subtitle1" >
-                        {get(post, "subtitle", null)}
-                    </Typography>
-                )}
+    ${seoFields}
+    ${assetFields}
+    ${richTextFields}
+  `;
 
-                {get(post, "image[0]", null) && (
-                    <div>
-                        <Image
-                            sizes={imageSizes}
-                            asset={get(post, "image[0]", null)}
-                            alt={get(post, "image[0].description") || get(post, "image[0].name", null)} />
-                    </div>
-                )}
-                <Typography component="div">
-                    <RichText
-                        {...props}
-                        richTextElement={get(post, "content", null)}
-                    />
-                </Typography>
+  const [post, setPost] = useState(null);
+  const [seo, setSeo] = useState(null);
+  const classes = useStyles();
+  const theme = useTheme();
+  const imageSizes = `${theme.breakpoints.values.md}px`;
 
-                <footer>
-                    <time>{get(post, "publishingDate", null) && new Date(get(post, "publishingDate", null)).toDateString()}</time>
-                    {get(post, "author.items[0]", null) &&
-                        (", by " + get(post, "author.items[0].firstName", null) + " " + get(post, "author.items[0].lastName", null))}
-                </footer>
-            </Container>
-        </Layout>
-    );
+  const { loading, error } = useQuery(
+    postPageQuery,
+    {
+      variables: { codename: props.codename },
+      onCompleted: (data) => {
+        setPost(data.post);
+        setSeo(getSeo(data.post._seo));
+      },
+    },
+    [props.codename]
+  );
+
+  if (error || loading || !post) {
+    return <GraphQLLoader error={error} loading={loading} />;
+  }
+
+  return (
+    <Layout {...props} seo={seo}>
+      <Container className={classes.root} maxWidth="md">
+        {get(post, "title", null) && (
+          <Typography variant="h1">{get(post, "title", null)}</Typography>
+        )}
+        {get(post, "subtitle", null) && (
+          <Typography variant="subtitle1">
+            {get(post, "subtitle", null)}
+          </Typography>
+        )}
+
+        {get(post, "image", null) && (
+          <div>
+            <Image
+              sizes={imageSizes}
+              asset={get(post, "image", null)}
+              alt={
+                get(post, "image.description") ||
+                get(post, "image.name", null)
+              }
+            />
+          </div>
+        )}
+        <Typography component="div">
+          <RichText {...props} richTextElement={get(post, "content", null)} />
+        </Typography>
+
+        <footer>
+          <time>
+            {get(post, "publishingDate", null) &&
+              new Date(get(post, "publishingDate", null)).toDateString()}
+          </time>
+          {get(post, "author", null) &&
+            ", by " +
+              get(post, "author.firstName", null) +
+              " " +
+              get(post, "author.lastName", null)}
+        </footer>
+      </Container>
+    </Layout>
+  );
 }
 
 export default Post;
